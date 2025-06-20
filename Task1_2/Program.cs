4d8f5a5d44c27dd0da51f2c2a767d4a5660d0a4f
Павел Мировщиков <pmirov@gmail.com>
2025-06-20 15:31:37 +0700

    Перенес задания в проект Task1_2

--- Task1_2/Program.cs
+++ Task1_2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Task1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
