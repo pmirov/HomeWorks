19f7e010000da4acf9cf313efa380b950ca6315b
Павел Мировщиков <pmirov@gmail.com>
2025-07-20 19:39:58 +0700

    fix

---
+++ Task1_10_1/Building.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_10_1
+{
+    internal class Building
+    {
+        protected string _address;
+        protected double _area;
+        protected int _yearBuilt;
+        protected int _buildingAge;
+
+
+        public int BuildingAge
+        {
+            get
+            {
+
+                _buildingAge = DateTime.Now.Year - _yearBuilt;
+                return _buildingAge;
+
+            }
+        }
+
+
+        public Building(string address, double area, int yearBuilt)
+        {
+            if (area <= 0)
+               area = 0;
+
+            if (yearBuilt <= 0)
+                yearBuilt = 0;
+
+            _address = address;
+            _area = area;
+            _yearBuilt = yearBuilt;
+
+        }
+
+        public virtual decimal CalculateTax()
+        {
+            decimal tax = (decimal)_area * 1000;
+            return tax;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"\nЗдание расположено по адресу: {_address}, \nплощадь здания: {_area} м2, \nгод постройки: {_yearBuilt} г., \nвозраст здания: {BuildingAge} л.");
+            Console.WriteLine("-------------------");
+        }
+    }
+}
---
+++ Task1_10_1/MultiBuilding.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_10_1
+{
+    internal sealed class MultiBuilding : Building
+    {
+        private int _floors;
+        private bool _hasElevator;
+        private double _areaPerFloor;
+        public double AreaPerFloor
+        {
+            get 
+            {
+                _areaPerFloor = _area / _floors;
+                return _areaPerFloor; 
+            }
+        }
+
+
+        public MultiBuilding(string address, double area, int yearBuilt, int floors, bool hasElevator)
+            : base(address, area, yearBuilt)
+        {
+            _floors = floors;
+            _hasElevator = hasElevator;
+        }
+
+        public override decimal CalculateTax()
+        {
+            decimal tax;
+            if (_hasElevator)
+            {
+                tax = (decimal)_area * 1000m * (1m + (_floors - 1) * 0.05m) + 5000;
+            }
+
+            else
+            {
+                tax = (decimal)_area * 1000m * (1m + (_floors - 1) * 0.05m);
+            }
+
+
+
+            return tax;
+        }
+
+        public override void DisplayInfo()
+        {
+            string yesNo = _hasElevator ? "да" : "нет";
+
+            base.DisplayInfo();
+            Console.Write($"\nколичество этажей: {_floors}, \nналичие лифта: {yesNo}, \nсредняя площадь этажа: {AreaPerFloor} м2");
+
+
+            Console.WriteLine("-------------------");
+           
+
+        }
+
+    }
+}
