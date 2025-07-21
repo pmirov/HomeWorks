926d4197ef3f7a1efa0ff9a6f2fa393fbe23090b
Павел Мировщиков <pmirov@gmail.com>
2025-07-21 20:50:19 +0700

    fix

---
+++ Task1_11_1/Dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_11_1
+{
+    internal class Dog : Animal
+    {
+        public override string Name { get => "Собака";  }
+
+        public override string Say()
+        {
+            return "Гав!";
+        }
+    }
+}
---
+++ Task_1_11_2/Bird.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_11_2
+{
+    internal class Bird : IFlyable
+    {
+        public int MaxAttitude { get; }
+
+        public Bird(int maxAttitude)
+        {
+            MaxAttitude = maxAttitude;
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine($"Лечу на высоте {MaxAttitude} метров");
+        }
+    }
+}
