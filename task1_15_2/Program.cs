88ec5627832b8573bc255db3a4f4510ec1d1ab8d
Павел Мировщиков <pmirov@gmail.com>
2025-07-29 23:28:48 +0700

    fix

---
+++ task1_15_2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace task1_15_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> inventoryStock = new Dictionary<string, int>();
+            inventoryStock.Add("А001", 10);
+            inventoryStock.Add("B205", 25);
+            inventoryStock.Add("C307", 15);
+            bool hasItem = inventoryStock.ContainsKey("B205");
+            Console.WriteLine($"Есть ли товар с номером B205?: {hasItem}");
+            inventoryStock["А001"] -= 2;
+            inventoryStock.TryGetValue("C307", out int count);
+            Console.WriteLine($"Количество наушников: {count}");
+            inventoryStock["B205"] += 5;
+            inventoryStock.Remove("C307");
+            Console.WriteLine("Текущие товары:");
+            foreach (var inventory in inventoryStock)
+            {
+                Console.WriteLine($"Артикул: {inventory.Key}, Количество: {inventory.Value}");
+            }
+
+            bool hasData = inventoryStock.Count< 0;
+
+            Console.WriteLine($"Список пуст? {hasData}") ;
+            inventoryStock.Clear();
+
+            Console.WriteLine($"Количество товаров после очистки: {inventoryStock.Count}");
+
+        }
+    }
+}
